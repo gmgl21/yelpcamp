b72431591fe84ce2cef1af4c69db87ada6976e3d
Gloria Lee <gmgl21@hotmail.com>
2017-07-19 00:57:44 +0000

    add mlab(mongolab)

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
     
-mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
+// mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
+mongoose.connect("mongodb://gloria:yelpcamp2017@ds063892.mlab.com:63892/yelpcamp", {useMongoClient: true});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
